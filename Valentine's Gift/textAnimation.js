06475c92001308a4c91e09f392d4cc66a30ac13b
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2024-08-09 14:10:57 -0300

    Reorganizado estrutura de pastas do repositório

--- Valentine's Gift/textAnimation.js
+++
@@ -1,7 +0,0 @@
-const elemento = document.querySelector('.message-container');
-
-elemento.addEventListener('animationend', () => {
-    elemento.style.animation = 'moving-flower-1 4s linear infinite';
-});
-
-elemento.style.animation = 'aparecer 6s forwards';
