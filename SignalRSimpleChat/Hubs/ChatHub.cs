dca8f177d6376b6d78cbc1350f434593c4626f12
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2024-08-09 19:17:48 -0300

    Projeto SignalR criado

---
+++ SignalRSimpleChat/Hubs/ChatHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Net.Mail;
+
+namespace SignalRSimpleChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        /// <summary>
+        /// Pega o Id do usuário e retorna
+        /// </summary>
+        /// <returns></returns>
+        public string GetUserConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+        /// <summary>
+        /// Envia uma mensagem ao usuário do id inserido
+        /// </summary>
+        /// <param name="userId">Id do usuário destinatário</param>
+        /// <param name="message">Mensagem a ser enviada</param>
+        /// <returns></returns>
+        public async Task SendMessage(string userId, string message)
+        {
+            await Clients.Client(userId).SendAsync("ReceiveMessage", message);
+        }
+
+
+        public override async Task OnConnectedAsync()
+        {
+            string? connectionId = GetUserConnectionId();
+            await Clients.All.SendAsync("ReceiveMessage", connectionId);
+
+            // Chame a base para continuar o fluxo de conexão padrão
+            await base.OnConnectedAsync();
+        }
+
+        // Evento executado quando um cliente se desconecta
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var connectionId = Context.ConnectionId;
+            await Clients.All.SendAsync("ReceiveMessage", "Um usuário se desconectou: " + connectionId);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
---
+++ SignalRSimpleChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRSimpleChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
